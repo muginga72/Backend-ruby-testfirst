340df5f04b0a2f39c51b355b25cd00f7df3c3ad6
muginga72 <lmj.muginga@gmail.com>
2023-10-29 18:53:05 -0400

    Week 4 assignment 00_Hello

--- 00_hello/hello.rb
+++ 00_hello/hello.rb
@@ -1,1 +1,18 @@
 #write your code here
+def hello
+  "Hello!"
+end
+
+def greet(name)
+  "Hello, #{name}!"
+end
+
+=begin
+OUTPUT:
+the hello function
+  says hello
+
+the greet function
+  says hello to someone
+  says hello to someone else
+=end
