e25215df72633e60dc7635c9ea03cafae4563708
muginga72 <lmj.muginga@gmail.com>
2023-10-30 17:49:37 -0400

    Week 5 assignment 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,33 @@
 #write your code here
+
+# Define the echo method that returns the input string.
+def echo(word)
+  word
+end
+
+# Define the shout method that returns the input string in uppercase.
+def shout(word)
+  word.upcase
+end
+
+# Define the repeat method that returns the input string 2 times.
+def repeat(word, times = 2)
+  ([word] * times).join(" ")
+end
+
+# Define the start_of_word method that returns the first n characters of a string.
+def start_of_word(word, length)
+  word[0, length]
+end
+
+# Define the first_word method that returns the first word of a sentence.
+def first_word(sentence)
+  sentence.split.first
+end
+
+# Define The titleize method that capitalizes every word in a title except for certain 
+# “little words” such as “and” and “the”.
+def titleize(title)
+  little_words = %w[and the over]
+  title.capitalize.split.map { |word| little_words.include?(word) ? word : word.capitalize }.join(" ")
+end
