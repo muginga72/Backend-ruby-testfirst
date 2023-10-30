5b0eadc02674e3c938dcaa4c153c06494d2bd004
muginga72 <lmj.muginga@gmail.com>
2023-10-30 17:53:59 -0400

    Week 5 assignment 00_hello

--- 00_hello/hello.rb
+++ 00_hello/hello.rb
@@ -6,13 +6,3 @@ end
 def greet(name)
   "Hello, #{name}!"
 end
-
-=begin
-OUTPUT:
-the hello function
-  says hello
-
-the greet function
-  says hello to someone
-  says hello to someone else
-=end
